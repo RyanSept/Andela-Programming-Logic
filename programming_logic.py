7966beafae43f964cb1846182c6af43afdfda1d8
Ryan Marvin <marvinryan@ymail.com>
2016-10-28 16:57:05 +0300

    prime_gen is working now

--- programming_logic.py
+++ programming_logic.py
@@ -1,13 +1,28 @@
+#this generates prime numbers up to given parameter 'end'
+#it returns a list
 def prime_generator(end):
-	pass
+    #empty list for storing generated numbers
+    l = []
+    #we loop
+    for n in range(0,end):
+        if is_prime(n):
+            l.append(n)
+    return l
 
-#this function takes an integer 'i' input and checks if it is prime
-def is_prime(i):
-	if i == 2 or i == 3:
-		return True
-	if i<2 or i%2 == 0:
-		return False
-	if n<9: 
-		return True
+#this checks if a given value is prime, returns a boolean
+def is_prime(num):
+    if num<2:
+        return False
+    if num==2:
+        return True
+    if num%3 == 0:
+        return False
+    else:
+        #we loop from 2 to the number checking if it's divisible by 2
+        #we return false if it is divisible, true if it's not
+        for i in range(2,num):
+            if num%2==0:
+                return False
+            return True
 
-	r = int(i**0.5)
+print prime_generator(10001)
