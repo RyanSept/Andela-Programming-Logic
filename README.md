ca764e6c16503eb137bcc34158ae2b23efcaff91
Ryan Marvin <marvinryan@ymail.com>
2016-10-28 21:54:14 +0300

    Edited README.md

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Andela-Programming-Logic
 The file programming_logic.py contains the functions prime_generator and fibonacci_generator
 
-prime_generator(end)
+**prime_generator(end)**
 
-This function takes 1 parameter that is the ceiling for all the prime numbers returned. eg. **prime_generator(4)** returns [2,3,4]
+This function takes 1 parameter that is the ceiling for all the prime numbers returned. eg. **prime_generator(4)** returns [2,3]
 
-fibonacci_generator(n) 
+**fibonacci_generator(end)**
 
-This function takes 1 parameter that returns all fibonacci numbers less than or equal to the parameter
+This function takes 1 parameter that returns all fibonacci numbers less than or equal to the parameter. eg. **fibonacci_generator(4)** returns [0,1,1,2,3]
--- programming_logic.py
+++ programming_logic.py
@@ -1,20 +1,22 @@
 #this generates prime numbers up to given parameter 'end'
 #it returns a list
 def prime_generator(end):
+    if type(end)!=type(1):
+        raise ValueError
+    if end<=1:
+        return []
     #empty list for storing generated numbers
-    l = []
+    l = [2,]
     #we loop
-    for n in range(0,end):
+    for n in range(0,end+1):
         if is_prime(n):
             l.append(n)
     return l
 
 #this checks if a given value is prime, returns a boolean
 def is_prime(num):
-    #don't loop if even
-    if num%2==0:
-        return False
-    if num<2:
+    #don't loop if even or less than 2
+    if num%2==0 or num<2:
         return False
     if num==2 or num==3:
         return True
@@ -22,23 +24,26 @@ def is_prime(num):
         #we loop from 2 to the number checking if it's divisible by 2
         #we return false if it is divisible, true if it's not
         for i in range(2,num):
-            if num%2==0:
+            if num%i==0:
                 return False
-            return True
+        return True
 
 
 #this generates all prime numbers up to n
 #for a non-fibonacci number, it returns a list with the last element as the
 #fibonacci closest to it eg. if n=4 return = [0,1,1,2,3]
-def fibonacci_generator(n):
-    if n<=1:
-        return [n]
+def fibonacci_generator(end):
+    if type(end)!=type(1):
+        raise ValueError
+    if end<=1:
+        return [end]
     next_fib = 0
     fibonacci_numbers = [0,1]
     #we loop through while our test case is not in the list of fibonacci numbers
-    while next_fib<n:
+    #and break when we reach or pass it
+    while next_fib<end:
         next_fib = fibonacci_numbers[-1]+fibonacci_numbers[-2]
-        if next_fib<=n:
+        if next_fib<=end:
             fibonacci_numbers.append(next_fib)
     return fibonacci_numbers
 
