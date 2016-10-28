c2f4255faad4e48f0fdd92c7a2023408c154eb03
Ryan Marvin <marvinryan@ymail.com>
2016-10-28 16:18:44 +0300

    Added is_prime function to programming_logic.py

--- programming_logic.py
+++ programming_logic.py
@@ -1,2 +1,13 @@
-def prime_generator():
+def prime_generator(end):
 	pass
+
+#this function takes an integer 'i' input and checks if it is prime
+def is_prime(i):
+	if i == 2 or i == 3:
+		return True
+	if i<2 or i%2 == 0:
+		return False
+	if n<9: 
+		return True
+
+	r = int(i**0.5)
