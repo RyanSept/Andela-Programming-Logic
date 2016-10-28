d169b6fb4b91b2206e91ae23ff8ed5941b173efe
Ryan Marvin <marvinryan@ymail.com>
2016-10-28 17:58:10 +0300

    Fibonacci complete

--- programming_logic.py
+++ programming_logic.py
@@ -13,10 +13,8 @@ def prime_generator(end):
 def is_prime(num):
     if num<2:
         return False
-    if num==2:
+    if num==2 or num==3:
         return True
-    if num%3 == 0:
-        return False
     else:
         #we loop from 2 to the number checking if it's divisible by 2
         #we return false if it is divisible, true if it's not
@@ -25,4 +23,18 @@ def is_prime(num):
                 return False
             return True
 
-print prime_generator(10001)
+print prime_generator(101)
+
+
+def fib(n):
+    if n<=1:
+        return n
+    next_fib = 0
+    fibonacci_numbers = [0,1]
+    while next_fib<n:
+        next_fib = fibonacci_numbers[-1]+fibonacci_numbers[-2]
+        fibonacci_numbers.append(next_fib)
+        print next_fib
+    return fibonacci_numbers
+
+        
