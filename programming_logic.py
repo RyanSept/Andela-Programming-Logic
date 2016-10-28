c67340c8713a206240658bb76c364c9cd12c53d3
Ryan Marvin <marvinryan@ymail.com>
2016-10-28 18:15:08 +0300

    Added comments to fibonacci_generator, optimized is_prime by checking if even before looping

--- programming_logic.py
+++ programming_logic.py
@@ -11,6 +11,8 @@ def prime_generator(end):
 
 #this checks if a given value is prime, returns a boolean
 def is_prime(num):
+    if num%2==0:
+        return False
     if num<2:
         return False
     if num==2 or num==3:
@@ -24,12 +26,15 @@ def is_prime(num):
             return True
 
 
-
+#this generates all prime numbers up to n
+#for a non-fibonacci number, it returns a list with the last element as the
+#fibonacci closest to it eg. if n=4 return = [0,1,1,2,3]
 def fibonacci_generator(n):
     if n<=1:
         return [n]
     next_fib = 0
     fibonacci_numbers = [0,1]
+    #we loop through while our test case is not in the list of fibonacci numbers
     while next_fib<n:
         next_fib = fibonacci_numbers[-1]+fibonacci_numbers[-2]
         if next_fib<=n:
